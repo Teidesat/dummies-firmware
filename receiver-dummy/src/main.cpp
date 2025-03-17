22fb7b208b5f136b8256d360b3dff2eacef7ea56
Sergio Tabares <alu0101124896@ull.edu.es>
2025-03-17 12:16:18 +0000

    Refactor code and apply standard code format #5

--- receiver-dummy/src/main.cpp
+++ receiver-dummy/src/main.cpp
@@ -5,34 +5,34 @@
 
 //==============================================================================
 
-#define SPI_MOSI 23
-#define SPI_MISO 19
-#define SPI_SCK 18
-#define SPI_CS 5
+#define SPI_MOSI (23)
+#define SPI_MISO (19)
+#define SPI_SCK (18)
+#define SPI_CS (5)
 
-#define SAMPLE_RATE 1000000  // 1 MHz
-#define BUFFER_SIZE 1024
-#define CIRCULAR_BUFFER_SIZE 4096  // Tamaño total del buffer circular
-
-//==============================================================================
-
-const char* ssid = "ESP32_NETWORK";
-const char* password = "password1234";
-const char* serverIP = "192.168.1.100";  // Raspberry Pi IP
-const int serverPort = 12345;
-
-WiFiClient client;
-spi_device_handle_t spi;
-uint16_t circular_buffer[CIRCULAR_BUFFER_SIZE];
-volatile size_t head = 0;
-volatile size_t tail = 0;
-volatile bool buffer_ready = false;
-unsigned long lastReconnectAttempt = 0;
+#define SAMPLE_RATE (1000000)  // 1 MHz
+#define BUFFER_SIZE (1024)
+#define CIRCULAR_BUFFER_SIZE (4096)  // Tamaño total del buffer circular
 
 //==============================================================================
 
-uint16_t calculateChecksum(uint16_t* data, size_t length);
-void IRAM_ATTR spi_callback(spi_transaction_t *trans);
+const String wifiSsid = "xxxxxx";
+const String wifiPassword = "xxxxxx";
+const auto * const serverHostname = "xxxxxx";  // Raspberry Pi IP
+constexpr int serverPort = 12345;
+
+WiFiClient wifiClient;
+spi_device_handle_t spiDeviceHandle;
+std::array<uint16_t, CIRCULAR_BUFFER_SIZE>circularBuffer;
+volatile size_t bufferHeadIndex = 0;
+volatile size_t bufferTailIndex = 0;
+volatile bool bufferIsReady = false;
+unsigned long lastReconnectionAttempt = 0;
+
+//==============================================================================
+
+uint16_t calculateChecksum(const uint16_t *pData, size_t length);
+void IRAM_ATTR spiTransmissionCompletedCallback(spi_transaction_t *trans);
 void setupSPI();
 void readADC();
 void setupWiFi();
@@ -50,86 +50,111 @@ void setup() {
 void loop() {
   reconnectWiFi();
   readADC();
-  if (buffer_ready) {
-    buffer_ready = false;
+
+  if (bufferIsReady) {
+    bufferIsReady = false;
   }
 }
 
 //==============================================================================
 
-uint16_t calculateChecksum(uint16_t* data, size_t length) {
+uint16_t calculateChecksum(const uint16_t *pData, const size_t length) {
   uint16_t checksum = 0;
+
   for (size_t i = 0; i < length; i++) {
-    checksum ^= data[i];
+    checksum ^= pData[i];
   }
+
   return checksum;
 }
 
-void IRAM_ATTR spi_callback(spi_transaction_t *trans) {
-  buffer_ready = true;
+void IRAM_ATTR spiTransmissionCompletedCallback(spi_transaction_t *trans) {
+  bufferIsReady = true;
 }
 
 void setupSPI() {
-  spi_bus_config_t buscfg = {
+  constexpr spi_bus_config_t spiBusConfig = {
       .mosi_io_num = SPI_MOSI,
       .miso_io_num = SPI_MISO,
       .sclk_io_num = SPI_SCK,
       .quadwp_io_num = -1,
       .quadhd_io_num = -1,
-      .max_transfer_sz = BUFFER_SIZE * sizeof(uint16_t)};
+      .max_transfer_sz = BUFFER_SIZE * sizeof(uint16_t)
+  };
 
-  spi_device_interface_config_t devcfg = {
+  constexpr spi_device_interface_config_t spiDeviceInterfaceConfig = {
       .mode = 0,
       .clock_speed_hz = SAMPLE_RATE * 16,  // 16 MHz SPI Clock
       .spics_io_num = SPI_CS,
-      .flags = SPI_DEVICE_HALFDUPLEX | SPI_DEVICE_NO_DUMMY | SPI_DEVICE_HALFDUPLEX,
+      .flags = SPI_DEVICE_HALFDUPLEX | SPI_DEVICE_NO_DUMMY,
       .queue_size = 1,
-      .pre_cb = NULL,
-      .post_cb = spi_callback};
+      .pre_cb = nullptr,
+      .post_cb = spiTransmissionCompletedCallback
+  };
 
-  spi_bus_initialize(HSPI_HOST, &buscfg, SPI_DMA_CH_AUTO);  // Activar canal DMA
-  spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
+  spi_bus_initialize(HSPI_HOST, &spiBusConfig, SPI_DMA_CH_AUTO);  // Activar canal DMA
+  spi_bus_add_device(HSPI_HOST, &spiDeviceInterfaceConfig, &spiDeviceHandle);
 }
 
 void readADC() {
-  spi_transaction_t trans = {};
-  memset(&trans, 0, sizeof(trans));
-  trans.length = BUFFER_SIZE * 16;  // 16 bits por muestra
-  trans.rx_buffer = &circular_buffer[head];
-  trans.flags = SPI_TRANS_USE_RXDATA;  // Asegurar uso de DMA
-  spi_device_queue_trans(spi, &trans, portMAX_DELAY);
-  head = (head + BUFFER_SIZE) % CIRCULAR_BUFFER_SIZE;
+  spi_transaction_t spiTransaction = {};
+
+  memset(&spiTransaction, 0, sizeof(spiTransaction));
+  spiTransaction.length = BUFFER_SIZE * 16;  // 16 bits por muestra
+  spiTransaction.rx_buffer = &circularBuffer[bufferHeadIndex];
+  spiTransaction.flags = SPI_TRANS_USE_RXDATA;  // Asegurar uso de DMA
+  spi_device_queue_trans(spiDeviceHandle, &spiTransaction, portMAX_DELAY);
+
+  bufferHeadIndex = (bufferHeadIndex + BUFFER_SIZE) % CIRCULAR_BUFFER_SIZE;
   sendBuffer();
 }
 
 void setupWiFi() {
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  WiFi.begin(wifiSsid, wifiPassword);
+  Serial.print("Connecting to WiFi...");
+
+  while (WiFiClass::status() != WL_CONNECTED) {
+    Serial.print('.');
     delay(500);
-    Serial.print(".");
   }
-  Serial.println("WiFi connected");
-  client.connect(serverIP, serverPort);
+
+  Serial.println("WiFi connection established.");
+  wifiClient.connect(serverHostname, serverPort);
 }
 
 void reconnectWiFi() {
-  if (WiFi.status() != WL_CONNECTED && (millis() - lastReconnectAttempt > 5000)) {
-    Serial.println("Reconnecting to WiFi...");
-    lastReconnectAttempt = millis();
-    WiFi.disconnect();
-    WiFi.begin(ssid, password);
-    client.connect(serverIP, serverPort);
+  if (
+      (WiFiClass::status() == WL_CONNECTED)
+      || (millis() - lastReconnectionAttempt <= 5000)
+  ) {
+    return;
   }
+
+  Serial.println("WiFi connection lost, trying to reconnect...");
+  lastReconnectionAttempt = millis();
+  WiFi.disconnect();
+  setupWiFi();
 }
 
 void sendBuffer() {
-  while (tail != head) {
-    uint16_t checksum = calculateChecksum(&circular_buffer[tail], BUFFER_SIZE);
-    if (client.connected()) {
-      client.write((uint8_t*)&circular_buffer[tail], BUFFER_SIZE * sizeof(uint16_t));
-      client.write((uint8_t*)&checksum, sizeof(checksum));
-      client.flush();  // Asegura que los datos se envíen inmediatamente
+  while (bufferTailIndex != bufferHeadIndex) {
+    uint16_t checksum = calculateChecksum(
+        &circularBuffer[bufferTailIndex],
+        BUFFER_SIZE
+    );
+
+    if (wifiClient.connected()) {
+      wifiClient.write(
+          reinterpret_cast<uint8_t *>(&circularBuffer[bufferTailIndex]),
+          BUFFER_SIZE * sizeof(uint16_t)
+      );
+      wifiClient.write(
+          reinterpret_cast<uint8_t *>(&checksum),
+          sizeof(checksum)
+      );
+      wifiClient.flush();  // Asegura que los datos se envíen inmediatamente
     }
-    tail = (tail + BUFFER_SIZE) % CIRCULAR_BUFFER_SIZE;
+
+    bufferTailIndex = (bufferTailIndex + BUFFER_SIZE) % CIRCULAR_BUFFER_SIZE;
   }
 }
--- transmitter-dummy/src/main.cpp
+++ transmitter-dummy/src/main.cpp
@@ -5,25 +5,28 @@
 
 //==============================================================================
 
-#define LIGHT_PIN (18) // ESP32 Pin to send light pulses
-#define DEBUG_LED_PIN (19) // ESP32 Pin for debugging
+#define LIGHT_PIN (18)  // ESP32 Pin to send light pulses
+#define DEBUG_LED_PIN (19)  // ESP32 Pin for debugging
+
+#define SAMPLE_RATE (1000000)  // 1 MHz
 
 //==============================================================================
 
-const char* wifiSsid = "xxxxxx";
-const char* wifiPassword = "xxxxxx";
+const String wifiSsid = "xxxxxx";
+const String wifiPassword = "xxxxxx";
 
-String apiServerBaseUrl = "http://xxxxxx:5000"; // Change to server's ip address
+const String apiServerBaseUrl = "http://xxxxxx:5000";  // Change to server's IP address
 
-String sendModeUrl = apiServerBaseUrl + "/get_send_mode";
-String blinkingFrequencyUrl = apiServerBaseUrl + "/get_blinking_frequency";
-String messageDataUrl = apiServerBaseUrl + "/get_message_data";
+const String sendModeUrl = apiServerBaseUrl + "/get_send_mode";
+const String blinkingFrequencyUrl = apiServerBaseUrl + "/get_blinking_frequency";
+const String messageDataUrl = apiServerBaseUrl + "/get_message_data";
 
 //==============================================================================
 
-void send_string(float blinkingFrequency, String& messageData);
-void send_binary(float blinkingFrequency, uint8_t* messageData, int dataSize);
-String get_request(WiFiClient& wifiClient, HTTPClient& httpClient, String target_url);
+void sendString(float blinkingFrequency, const String& messageData);
+void sendBinary(float blinkingFrequency, const uint8_t* messageData, unsigned int dataSize);
+String getRequest(WiFiClient& wifiClient, HTTPClient& httpClient, const String& targetUrl);
+void setupWiFi();
 
 //==============================================================================
 
@@ -33,98 +36,99 @@ void setup() {
 
   Serial.begin(9600);
 
-  WiFi.begin(wifiSsid, wifiPassword);
-  Serial.print("Connecting to WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(500);
-  }
-  Serial.println("WiFi connection established.");
+  setupWiFi();
 }
 
 void loop() {
   WiFiClient wifiClient;
   HTTPClient httpClient;
 
-  String sendMode;
-  sendMode = get_request(wifiClient, httpClient, sendModeUrl);
+  const String sendMode = getRequest(wifiClient, httpClient, sendModeUrl);
+  Serial.println("Send mode: " + sendMode);
 
   if (sendMode == "Error") {
     Serial.println("Error getting send mode");
     return;
   }
-  else {
-    Serial.println("Send mode: " + sendMode);
-  }
 
   if (sendMode == "send_string") {
-    String blinkingFrequency = get_request(wifiClient, httpClient, blinkingFrequencyUrl);
+    const String blinkingFrequency = getRequest(wifiClient, httpClient, blinkingFrequencyUrl);
+    Serial.println("Frequency: " + blinkingFrequency);
+
     if (blinkingFrequency == "Error") {
       Serial.println("Error getting blinking frequency");
       return;
     }
-    else {
-      Serial.println("Frequency: " + blinkingFrequency);
-    }
 
-    String messageData = get_request(wifiClient, httpClient, messageDataUrl);
+    const String messageData = getRequest(wifiClient, httpClient, messageDataUrl);
+    Serial.println("Data: " + messageData);
+
     if (messageData == "Error") {
       Serial.println("Error getting message data");
       return;
     }
-    else {
-      Serial.println("Data: " + messageData);
-    }
 
-    send_string(blinkingFrequency.toFloat(), messageData);
+    sendString(blinkingFrequency.toFloat(), messageData);
   }
 
   if (sendMode == "send_binary") {
-    String blinkingFrequency = get_request(wifiClient, httpClient, blinkingFrequencyUrl);
+    const String blinkingFrequency = getRequest(wifiClient, httpClient, blinkingFrequencyUrl);
+    Serial.println("Frequency: " + blinkingFrequency);
+
     if (blinkingFrequency == "Error") {
       Serial.println("Error getting blinking frequency");
       return;
     }
-    else {
-      Serial.println("Frequency: " + blinkingFrequency);
-    }
 
-    String messageData = get_request(wifiClient, httpClient, messageDataUrl);
+    String messageData = getRequest(wifiClient, httpClient, messageDataUrl);
+    Serial.println("Data: " + messageData);
+
     if (messageData == "Error") {
       Serial.println("Error getting message data");
       return;
     }
-    else {
-      Serial.println("Data: " + messageData);
-    }
 
-    int dataSize = messageData.length();
+    const unsigned int dataSize = messageData.length();
     uint8_t dataBytes[dataSize];
     for (int i = 0; i < dataSize; i++) {
       dataBytes[i] = messageData[i] - '0';
     }
 
-    send_binary(blinkingFrequency.toFloat(), dataBytes, dataSize);
+    sendBinary(blinkingFrequency.toFloat(), dataBytes, dataSize);
   }
 }
 
 //==============================================================================
 
-void send_string(float blinkingFrequency, String& messageData) {
+void setupWiFi() {
+  WiFi.begin(wifiSsid, wifiPassword);
+  Serial.print("Connecting to WiFi...");
+
+  while (WiFiClass::status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(500);
+  }
+
+  Serial.println("WiFi connection established.");
+}
+
+void sendString(const float blinkingFrequency, const String& messageData) {
   digitalWrite(DEBUG_LED_PIN, HIGH);
   Serial.println("Sending message data");
 
-  unsigned long bitWaitTime = 1000000 / blinkingFrequency; // Time in microseconds for each bit
+  const auto bitWaitTime = static_cast<unsigned long>(SAMPLE_RATE / blinkingFrequency);  // Time in microseconds for each bit
   unsigned long startTime = micros();
 
-  int dataSize = messageData.length();
-  for (int i = 0; i < dataSize; i++) {
-    uint8_t byte = messageData[i]; // Character of the message data string
+  const unsigned int dataSize = messageData.length();
+  for (unsigned int i = 0; i < dataSize; i++) {
+    const uint8_t byte = messageData[i];  // Character of the message data string
 
     for (int j = 7; j >= 0; j--) {
-      digitalWrite(LIGHT_PIN, (byte >> j) & 1); // Send the bit
+      digitalWrite(LIGHT_PIN, (byte >> j) & 1);  // Send the bit
 
-      while ((micros() - startTime) < bitWaitTime); // Wait for the bit time to pass
+      while ((micros() - startTime) < bitWaitTime) {
+        // Wait for the bit time to pass
+      }
       startTime += bitWaitTime;
     }
   }
@@ -133,17 +137,19 @@ void send_string(float blinkingFrequency, String& messageData) {
   digitalWrite(DEBUG_LED_PIN, LOW);
 }
 
-void send_binary(float blinkingFrequency, uint8_t* messageData, int dataSize) {
+void sendBinary(const float blinkingFrequency, const uint8_t* messageData, const unsigned int dataSize) {
   digitalWrite(DEBUG_LED_PIN, HIGH);
   Serial.println("Sending message data");
 
-  unsigned long bitWaitTime = 1000000 / blinkingFrequency; // Time in microseconds for each bit
+  const auto bitWaitTime = static_cast<unsigned long>(SAMPLE_RATE / blinkingFrequency);  // Time in microseconds for each bit
   unsigned long startTime = micros();
 
   for (int i = 0; i < dataSize; i++) {
-    digitalWrite(LIGHT_PIN, messageData[i] ? HIGH : LOW); // Send the bit
+    digitalWrite(LIGHT_PIN, messageData[i] ? HIGH : LOW);  // Send the bit
 
-    while ((micros() - startTime) < bitWaitTime); // Wait for the bit time to pass
+    while ((micros() - startTime) < bitWaitTime) {
+      // Wait for the bit time to pass
+    }
     startTime += bitWaitTime;
   }
 
@@ -151,29 +157,28 @@ void send_binary(float blinkingFrequency, uint8_t* messageData, int dataSize) {
   digitalWrite(DEBUG_LED_PIN, LOW);
 }
 
-String get_request(WiFiClient& wifiClient, HTTPClient& httpClient, String target_url) {
+String getRequest(WiFiClient& wifiClient, HTTPClient& httpClient, const String& targetUrl) {
   Serial.println("Sending http request");
-  if (httpClient.begin(wifiClient, target_url)) {
+
+  if (httpClient.begin(wifiClient, targetUrl)) {
     Serial.println("URL initialized");
   }
 
-  int responseCode = httpClient.GET();
-  if (responseCode > 0) {
-    Serial.printf("[HTTP] GET... code: %d", responseCode);
+  const int responseCode = httpClient.GET();
 
-    if (responseCode == HTTP_CODE_OK) {
-      String responsePayload = httpClient.getString();
-      Serial.println("Received payload: " + responsePayload);
-      return responsePayload;
-    }
-    else {
-      Serial.printf("ERROR: %s", httpClient.errorToString(responseCode).c_str());
-      return "Error";
-    }
-  }
-  else {
-    Serial.printf("[HTTP] GET... failed, error: %s\n", httpClient.errorToString(responseCode).c_str());
+  if (responseCode != HTTP_CODE_OK) {
+    Serial.printf(
+        "[HTTP] GET... failed, error: %s\n",
+        HTTPClient::errorToString(responseCode).c_str()
+    );
+
+    return "Error";
   }
 
-  return "Error";
+  Serial.printf("[HTTP] GET... code: %d", responseCode);
+
+  String responsePayload = httpClient.getString();
+  Serial.println("Received payload: " + responsePayload);
+
+  return responsePayload;
 }
